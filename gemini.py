354193aa4a4077f7f0fee21efe29baf3051f0d66
Deepak kori <111627339+Deepakkori45@users.noreply.github.com>
2024-03-24 21:01:46 +0530

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -44,16 +44,8 @@ gemini_response = st.session_state.chat_session.send_message(user_prompt_with_co
 # Input field for user's message
 user_prompt = st.chat_input("Ask Gemini-Pro...")
 if user_prompt:
-    # Check if it's the first user's message in the session
-    if "first_message_sent" not in st.session_state:
-        # Prepend the predefined prompt to the user's first message for model processing
-        user_prompt_with_context = predefined_prompt + "respond to this as how can i assist you today?" 
-        # Mark the first message as sent in the session state
-        st.session_state.first_message_sent = True
-    else:
-        # For subsequent messages, just use the user's input for model processing
-        user_prompt_with_context = user_prompt
-
+    # For subsequent messages, just use the user's input for model processing
+    # user_prompt_with_context = user_prompt
     # Always add user's original message to chat and display it
     st.chat_message("user").markdown(user_prompt)
 
